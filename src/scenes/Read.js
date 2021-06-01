2be2dd1a8af413536cb520a7ee8e833a4d9ebd68
Ethan Grossbach <egrossba@ucsc.edu>
2021-06-01 13:37:02 -0700

    tween like a boss

--- src/scenes/Read.js
+++ src/scenes/Read.js
@@ -7,10 +7,19 @@ class Read extends Phaser.Scene {
         this.scene.bringToTop(this);
         this.scenes = game.scene.getScenes(false);
 
-        this.add.sprite(game.config.width/2, game.config.height/2, "bunny").setOrigin(0.5).setScale(0.8);
+        this.paper = this.add.sprite(game.config.width/2, game.config.height/2, "bunny").setOrigin(0.5).setScale(0.8);
         this.add.text(game.config.width/2, game.config.height/2, newspaperText).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height*14/15, '[space] to exit').setOrigin(0.5);
 
+        this.tweens.add({
+            targets: this.paper,
+            alpha: { from: 0, to: 1 },
+            scale: { from: 0.1, to: 0.8 },
+            angle: { from: 0, to: 360 },
+            ease: 'Sine.easeInOut',
+            duration: 500
+        });
+        
         // space to close bunny
         this.input.keyboard.once('keydown-SPACE', () => {
             this.unpause();
