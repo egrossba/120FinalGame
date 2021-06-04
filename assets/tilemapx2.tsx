a9c5c97f604641ac5bae75c870a4bbe75fd99bd5
ian11207 <ianbobean@gmail.com>
2021-06-03 18:55:50 -0700

    level 6 added
    title

---
+++ assets/tilemapx2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tilemapx2" tilewidth="32" tileheight="32" tilecount="112" columns="14">
+ <image source="../../../../Downloads/tilemapx2.png" width="448" height="256"/>
+</tileset>
