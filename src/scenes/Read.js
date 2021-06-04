c7f08dd71ef9879f4bc77b38441488ff3650714d
Yeni Wang <ywang887@ucsc.edu>
2021-06-04 13:55:30 -0700

    add newspaper titles

--- src/scenes/Read.js
+++ src/scenes/Read.js
@@ -8,17 +8,16 @@ class Read extends Phaser.Scene {
         this.scenes = game.scene.getScenes(false);
 
         this.paper = this.add.sprite(game.config.width/2, game.config.height/2, newsIssue[levelNum]).setOrigin(0.5);
-        this.papers = this.add.sprite(game.config.width/2, game.comfig.height/2, newsTitle[levelNum]).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height - 45, newsTitle[levelNum]).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height - 20, '[space] to exit').setOrigin(0.5);
-        // add text for each newspaper here
-
+        
         this.tweens.add({
-            targets: this.paper,
+            targets: this.paper, 
             alpha: { from: 0, to: 1 },
             scale: { from: 0.1, to: 0.25 },
             angle: { from: 0, to: 360 },
             ease: 'Sine.easeInOut',
-            duration: 500
+            duration: 500 
         });
         
         // space to close newspaper
