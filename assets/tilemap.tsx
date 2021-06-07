913c43341916654ff4f3a3c5684b6004f249d728
Ethan Grossbach <egrossba@ucsc.edu>
2021-06-07 00:51:32 -0700

    cleaning up garbage

--- assets/tilemap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="tilemap" tilewidth="32" tileheight="32" tilecount="252" columns="21">
- <image source="../../../../Downloads/tilemap.png" width="672" height="384"/>
-</tileset>
--- assets/tilemapx2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="tilemap" tilewidth="32" tileheight="32" tilecount="112" columns="14">
- <image source="tilemap.png" width="448" height="256"/>
-</tileset>
