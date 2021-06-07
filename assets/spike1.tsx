9a5c993405046419675df2f797d31ff2c04f2794
ian11207 <ianbobean@gmail.com>
2021-06-07 00:35:57 -0700

    changed levels 1-9
    added new spikes and qol changes (lvl 9 and lvl 6)

---
+++ assets/spike1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="spike1" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="../../../../Downloads/spike1.png" width="96" height="48"/>
+</tileset>
