6ecd2fc71a12a13ec63c2b3d50b085d01a15a168
Ethan Grossbach <egrossba@ucsc.edu>
2021-06-06 19:07:01 -0700

    interactive news list

--- src/scenes/NewspaperAccess.js
+++ src/scenes/NewspaperAccess.js
@@ -5,14 +5,30 @@ class NewspaperAccess extends Phaser.Scene {
 
     create(){
         // show up the newspaper list menu title
-        this.add.sprite(40 + 400, 90, 'newssign').setScale(0.17).setAlpha(1).setInteractive();
+        this.add.sprite(game.config.width/2, 50, 'newssign').setScale(0.1).setInteractive();
 
         // show up the newspaper list
         this.newsList = this.add.group();
         for(let i = 0; i < newsAccess.length; i++){
-        if(newsAccess[i]){
-            this.newsList.add(this.add.sprite(game.config.width/2 + 210*i, game.config.height/2, newsIssue[i]).setScale(0.095).setAlpha(1));
+            if(newsAccess[i]){
+                let n;
+                if(i < 4){
+                    n = this.add.sprite(120 + game.config.width*i/4, game.config.height/3, newsIssue[i]).setScale(0.075).setOrigin(0.5);
+                }
+                else{
+                    n = this.add.sprite(120 + game.config.width*i/4, game.config.height*2/3, newsIssue[i]).setScale(0.075).setOrigin(0.5);
+                }
+                n.setInteractive()
+                .on('pointerover', () => {
+                    n.setTint(0x955FEF);
+                }) // change pointer colours
+                .on('pointerout', () => {
+                    n.setTint(0xFFFFFF);
+                })
+                .on('pointerdown', () => {
+                    
+                });
+            }
         }
-    }
     }
 }
